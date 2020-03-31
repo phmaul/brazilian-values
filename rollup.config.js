cbbabc331878d8d01c8c67e627675165250b5c80
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-03-31 15:51:18 -0300

    :fire: remove rollup-plugin-license and use banner property instead

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import pkg from './package.json';
-import license from 'rollup-plugin-license';
 import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
@@ -7,47 +6,46 @@ import typescript from '@rollup/plugin-typescript';
  * A `string` used as copyright notice that is injected in generated bundles. It
  * contains package's name, version, author and license information.
  */
-const banner = `
-${pkg.name} v${pkg.version}
-(c) ${pkg.author.name} <${pkg.author.email}> (${pkg.author.url})
-Released under the MIT License.
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * (c) ${pkg.author.name} <${pkg.author.email}> (${pkg.author.url})
+ * Released under the MIT License.
+ */
 `;
 
 /**
  * An object with all configuration for `Rollup.js`.
- * @type {import('rollup').RollupFileOptions}
+ * @type {import('rollup').RollupOptions}
  */
 const options = {
   input: './src/brazilian-values.ts',
   plugins: [
-    license({
-      banner: {
-        content: banner,
-        commentStyle: 'ignored',
-      },
-    }),
     typescript({
       tsconfig: 'tsconfig.build.json',
     }),
   ],
   output: [
     {
+      banner,
       file: './dist/brazilian-values.esm.js',
       format: 'esm',
       sourcemap: true,
     },
     {
+      banner,
       file: './dist/brazilian-values.js',
       format: 'commonjs',
       sourcemap: true,
     },
     {
+      banner,
       file: './dist/brazilian-values.umd.js',
       name: 'BrazilianValues',
       format: 'umd',
       sourcemap: true,
     },
     {
+      banner,
       file: './dist/brazilian-values.umd.min.js',
       name: 'BrazilianValues',
       format: 'umd',
